bd0aaf0d899b27aa8866a97f52b7451cdad0fc38
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-12-01 20:33:14 +0300

    add redux-saga

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,22 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import createSagaMiddleware from '@redux-saga/core';
 import serviceManageProject from 'store/projects/reducers';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const reducers = combineReducers({
   manageProject: serviceManageProject, 
 });
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  composeEnhancers(
+    applyMiddleware(sagaMiddleware)
+    ),
+);
+
+sagaMiddleware.run(saga);
 
 export default store;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
-import  express from 'express';
+const express = require('express');
 const app = express()
 const port = process.env.PORT || 3001;
+const initialData = require('./database/initial');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/static', express.static(__dirname + '/public'));
+
+// Получение начальных данных
+
+app.get('/api/progects', (req, res) => {
+  res.send(initialData);
+});
+
+app.post('/api/progects', (req, res) => {
+  console.log(req)
+  res.send(initialData);
+});
+
+app.put('/api/progect/:id', (req, res) => {
+  res.send(initialData);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
