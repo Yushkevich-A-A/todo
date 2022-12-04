8bee9e77e8513cc26393af23d0d46f93717f8b83
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-12-05 01:33:19 +0300

    lint

--- client/src/components/TaskInformation/TaskInformationBlocks/Name/index.js
+++ client/src/components/TaskInformation/TaskInformationBlocks/Name/index.js
@@ -1,20 +1,34 @@
 import React, { useState }  from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Input from 'components/elements/Input';
 import { useDispatch } from 'react-redux';
 
 const Container = styled.div`
   margin-bottom: 10px;
+  & input {
+    background-color: white;
+    ${ props => {
+      return props.errorMark && css`
+      border-radius: 2pt;
+      box-shadow: 0 0 0 1pt red;
+     `
+   } }
 `;
 
 function Name(props) {
   const { task } = props;
   const dispatch = useDispatch();
   const [ name, setName ] = useState(task.name);
+  const [ errorMark, setError ] = useState(false);
 
   const handleOnBlur = () => {
     if ( task.name === name ) {
+      return;
+    }
+    if ( name === '' || name.length < 10) {
+      setName(task.name);
+      setError(true);
       return;
     }
     dispatch({
@@ -28,7 +42,7 @@ function Name(props) {
   }
 
   return (
-    <Container>
+    <Container onClick={() => {errorMark && setError(false)}} errorMark={errorMark}>
       <Input type='main' value={name} name='name' id='name' handleChange={handleChange} handleOnBlur={handleOnBlur} placeholder='Введите названи'/>
     </Container>
   )
